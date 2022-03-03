0f5c3c6e0a851923a45a21833cfeee876d5c5ea2
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-03 03:52:22 +0000

    init: repo

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+games { "gta5" }
+
+author "Fraser Watt (https://github.com/FrazzIe)"
+description ""
+version "0.1"
+
+ui_page "ui/dist/index.html"
+
+files {
+	"ui/dist/*.*",
+}
