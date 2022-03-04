2a8771b2340d1b9f875dacc13e51f821c5468ad6
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-04 21:21:04 +0000

    feat(ui/MdtLogin): basic login form with logo

--- ui/src/components/MdtLogin.vue
+++ ui/src/components/MdtLogin.vue
@@ -1,18 +1,59 @@
 <style>
+	.app-login-container
+	{
+		position: absolute;
 
+		width: 25%;
+
+		top: 50%;
+		left: 50%;
+
+		transform: translate(-50%, -50%);
+	}
+
+	.app-login-container .logo
+	{
+		display: flex;
+
+		justify-content: center;
+	}
 </style>
 
 <template>
-	<div>Login</div>
+	<div class="app-login-container">
+		<div class="logo">
+			<n-icon size="8em">
+				<i-tabler-user/>
+			</n-icon>
+		</div>
+		<n-form :model="loginForm">
+			<n-form-item label="Username" path="usernmae">
+				<n-input v-model:value="loginForm.username" placeholder="Enter username"></n-input>
+			</n-form-item>
+			<n-form-item label="Password" path="password">
+				<n-input type="password" v-model:value="loginForm.password" placeholder="Enter password"></n-input>
+			</n-form-item>
+			<n-form-item>
+				<n-button>Login</n-button>
+			</n-form-item>
+		</n-form>
+	</div>
 </template>
 
 <script lang="ts">
-	import { defineComponent } from "vue";
+	import { defineComponent, ref } from "vue";
 
 	export default defineComponent({
 		setup()
 		{
-			return { }
+			const loginForm = ref({
+				username: "",
+				password: ""
+			});
+
+			return {
+				loginForm
+			}
 		}
 	});
 </script>
