4d86c04abe45206b5814e62c4c17ae16f66db504
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-04 19:55:53 +0000

    feat(ui/MdtHomeTab): map vuex state

--- ui/src/components/tabs/MdtHomeTab.vue
+++ ui/src/components/tabs/MdtHomeTab.vue
@@ -8,8 +8,13 @@
 
 <script lang="ts">
 	import { defineComponent } from "vue";
+	import { mapState } from "vuex";
 
 	export default defineComponent({
+		computed:
+		{
+			...mapState(["auth"])
+		},
 		setup()
 		{
 			return { }
