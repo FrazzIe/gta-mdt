18d51bf098b8399fc2eee9c10b9b1e60436fb315
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-04 00:45:08 +0000

    feat(ui/store): tabs array property

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -1,10 +1,14 @@
 import { InjectionKey } from "vue";
 import { createStore, useStore as baseUseStore, Store } from "vuex";
 
+// interfaces
+import TabNavItem from "./interfaces/TabNavItem";
+
 // define your typings for the store state
 export interface State
 {
 	auth: boolean;
+	tabs: TabNavItem[];
 }
 
 // define injection key
@@ -13,7 +17,11 @@ export const key: InjectionKey<Store<State>> = Symbol();
 export const store = createStore<State>({
 	state:
 	{
-		auth: false
+		auth: false,
+		tabs:
+		[
+			{ label: "Home", component: "mdt-home", closable: false }
+		]
 	}
 });
 
--- ui/src/vuex.d.ts
+++ ui/src/vuex.d.ts
@@ -1,12 +1,16 @@
 
 import { Store } from "vuex"
 
+// interfaces
+import TabNavItem from "./interfaces/TabNavItem";
+
 declare module "@vue/runtime-core"
 {
 	// declare your own store states
 	interface State
 	{
 		auth: boolean;
+		tabs: TabNavItem[];
 	}
 
 	// provide typings for `this.$store`
