7767030ba727afb320ffabf9e422d92a19910fda
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-09 17:20:06 +0000

    feat(ui): empty MdtProfileTab component

---
+++ ui/src/components/tabs/MdtProfileTab.vue
@@ -0,0 +1,28 @@
+<style>
+
+</style>
+
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		props:
+		{
+			tabId:
+			{
+				type: String,
+				required: true
+			}
+		},
+		setup()
+		{
+			return { };
+		}
+	});
+</script>
