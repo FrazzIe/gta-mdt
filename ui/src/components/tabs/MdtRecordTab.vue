8204296ab4b988c4ff8d9d437501c5fe65f27007
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-13 19:45:57 +0000

    feat(ui/MdtRecordTab): avatar / info header

--- ui/src/components/tabs/MdtRecordTab.vue
+++ ui/src/components/tabs/MdtRecordTab.vue
@@ -1,13 +1,53 @@
 <style>
 	.record-tab
 	{
+		padding: var(--app-tabs-content-padding);
+	}
 
+	/* Header Area */
+
+	.record-tab--header
+	{
+		position: relative;
+		display: flex;
+
+		gap: 1rem;
+	}
+
+	.record-tab--header  .record-avatar
+	{
+		--n-avatar-size-override: 6rem;
+	}
+
+	.record-tab--header  .record-info
+	{
+		display: flex;
+
+		flex-direction: column;
+		flex-grow: 1;
+	}
+
+	.record-tab--header .record-username
+	{
+		font-size: 1.5rem;
 	}
 </style>
 
 <template>
 	<div class="record-tab">
+		<div class="record-tab--header">
+			<n-avatar class="record-avatar">
+				<n-icon>
+					<i-tabler-user/>
+				</n-icon>
+			</n-avatar>
 
+			<div class="record-info">
+				<span class="record-username">Eric Milton</span>
+				<span>ID: 12D7237237</span>
+				<span>Male — 15/07/1999 (22)</span>
+			</div>
+		</div>
 	</div>
 </template>
 
