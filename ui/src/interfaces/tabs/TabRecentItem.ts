b6c8247be08be041b908c38da541caf84e9382dd
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2022-03-19 18:45:37 +0000

    feat(ui/interfaces): TabRecentItem interface

---
+++ ui/src/interfaces/tabs/TabRecentItem.ts
@@ -0,0 +1,9 @@
+import TabComponent from "../../types/TabComponent";
+import IconComponent from "../../types/IconComponent";
+
+export default interface TabRecentItem
+{
+	title: string;
+	component: TabComponent;
+	icon: IconComponent
+}
